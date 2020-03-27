537446405e35a1a94176b1e113916a27d221e194
James Zhan <zhiqiangzhan@gmail.com>
2020-03-28 03:58:47 +0800

    commit v0.01

--- Makefile
+++ Makefile
@@ -1,17 +1,22 @@
 #
-# if you want the ram-disk device, define this to be the
-# size in blocks.
+# Makefile for linux.
+# If you don't have '-mstring-insns' in your gcc (and nobody but me has :-)
+# remove them from the CFLAGS defines.
 #
-AS86	=as86 -0 -a
-LD86	=ld86 -0
+
+AS86	=as -0 -a
+CC86	=cc -0
+LD86	=ld -0
 
 AS	=gas
 LD	=gld
 LDFLAGS	=-s -x -M
-CC	=gcc $(RAMDISK)
-CFLAGS	=-Wall -O -fstrength-reduce -fomit-frame-pointer \
--fcombine-regs 
-CPP	=cpp -nostdinc -Iinclude
+CC	=gcc
+CFLAGS	=-Wall -O -fstrength-reduce -fomit-frame-pointer -fcombine-regs
+CPP	=gcc -E -nostdinc -Iinclude
+
+ARCHIVES=kernel/kernel.o mm/mm.o fs/fs.o
+LIBS	=lib/lib.a
 
 .c.s:
 	$(CC) $(CFLAGS) \
@@ -25,30 +30,67 @@ CPP	=cpp -nostdinc -Iinclude
 all:	Image
 
 Image: boot/boot tools/system tools/build
-	tools/build boot/boot tools/system  > Image
+	tools/build boot/boot tools/system > Image
 	sync
-
-disk: Image
-	dd bs=8192 if=Image of=/dev/fd0
-	sync;sync;sync
 
 tools/build: tools/build.c
 	$(CC) $(CFLAGS) \
 	-o tools/build tools/build.c
+	chmem +65000 tools/build
 
 boot/head.o: boot/head.s
 
-tools/system:	boot/head.o 
-	$(LD) $(LDFLAGS) boot/head.o  -o tools/system > System.map
+tools/system:	boot/head.o init/main.o \
+		$(ARCHIVES) $(LIBS)
+	$(LD) $(LDFLAGS) boot/head.o init/main.o \
+	$(ARCHIVES) \
+	$(LIBS) \
+	-o tools/system > System.map
 
+kernel/kernel.o:
+	(cd kernel; make)
 
+mm/mm.o:
+	(cd mm; make)
 
-boot/boot:	boot/boot.s
-	$(AS86) -o boot/boot.o boot/boot.s
+fs/fs.o:
+	(cd fs; make)
+
+lib/lib.a:
+	(cd lib; make)
+
+boot/boot:	boot/boot.s tools/system
+	(echo -n "SYSSIZE = (";ls -l tools/system | grep system \
+		| cut -c25-31 | tr '\012' ' '; echo "+ 15 ) / 16") > tmp.s
+	cat boot/boot.s >> tmp.s
+	$(AS86) -o boot/boot.o tmp.s
+	rm -f tmp.s
 	$(LD86) -s -o boot/boot boot/boot.o
 
-
 clean:
-	rm -f Image System.map tmp_make core boot/boot 
-	rm -f init/*.o tools/system tools/build boot/*.o
+	rm -f Image System.map tmp_make boot/boot core
+	rm -f init/*.o boot/*.o tools/system tools/build
+	(cd mm;make clean)
+	(cd fs;make clean)
+	(cd kernel;make clean)
+	(cd lib;make clean)
 
+backup: clean
+	(cd .. ; tar cf - linux | compress16 - > backup.Z)
+	sync
+
+dep:
+	sed '/\#\#\# Dependencies/q' < Makefile > tmp_make
+	(for i in init/*.c;do echo -n "init/";$(CPP) -M $$i;done) >> tmp_make
+	cp tmp_make Makefile
+	(cd fs; make dep)
+	(cd kernel; make dep)
+	(cd mm; make dep)
+
+### Dependencies:
+init/main.o : init/main.c include/unistd.h include/sys/stat.h \
+  include/sys/types.h include/sys/times.h include/sys/utsname.h \
+  include/utime.h include/time.h include/linux/tty.h include/termios.h \
+  include/linux/sched.h include/linux/head.h include/linux/fs.h \
+  include/linux/mm.h include/asm/system.h include/asm/io.h include/stddef.h \
+  include/stdarg.h include/fcntl.h 
--- boot/head.s
+++ boot/head.s
@@ -1,107 +1,59 @@
 /*
  *  head.s contains the 32-bit startup code.
- *  Two L3 task multitasking. The code of tasks are in kernel area, 
- *  just like the Linux. The kernel code is located at 0x10000. 
+ *
+ * NOTE!!! Startup happens at absolute address 0x00000000, which is also where
+ * the page directory will exist. The startup code will be overwritten by
+ * the page directory.
  */
-KRN_BASE 	= 0x10000
-TSS0_SEL	= 0x20
-LDT0_SEL	= 0x28
-TSS1_SEL	= 0X30
-LDT1_SEL	= 0x38
-
 .text
+.globl _idt,_gdt,_pg_dir
+_pg_dir:
 startup_32:
 	movl $0x10,%eax
 	mov %ax,%ds
 	mov %ax,%es
 	mov %ax,%fs
 	mov %ax,%gs
-	lss stack_ptr,%esp
-
-# setup base fields of descriptors.
-	movl $KRN_BASE, %ebx
-	movl $gdt, %ecx
-	lea tss0, %eax
-	movl $TSS0_SEL, %edi	
-	call set_base
-	lea ldt0, %eax
-	movl $LDT0_SEL, %edi
-	call set_base
-	lea tss1, %eax
-	movl $TSS1_SEL, %edi 
-	call set_base
-	lea ldt1, %eax
-	movl $LDT1_SEL, %edi
-	call set_base
-
+	lss _stack_start,%esp
 	call setup_idt
 	call setup_gdt
 	movl $0x10,%eax		# reload all the segment registers
-	mov %ax,%ds		# after changing gdt. 
-	mov %ax,%es
+	mov %ax,%ds		# after changing gdt. CS was already
+	mov %ax,%es		# reloaded in 'setup_gdt'
 	mov %ax,%fs
 	mov %ax,%gs
-	lss stack_ptr,%esp
+	lss _stack_start,%esp
+	xorl %eax,%eax
+1:	incl %eax		# check that A20 really IS enabled
+	movl %eax,0x000000
+	cmpl %eax,0x100000
+	je 1b
+	movl %cr0,%eax		# check math chip
+	andl $0x80000011,%eax	# Save PG,ET,PE
+	testl $0x10,%eax
+	jne 1f			# ET is set - 387 is present
+	orl $4,%eax		# else set emulate bit
+1:	movl %eax,%cr0
+	jmp after_page_tables
 
-# setup up timer 8253 chip.
-	movb $0x36, %al
-	movl $0x43, %edx
-	outb %al, %dx
-	movl $11930, %eax        # timer frequency 100 HZ 
-	movl $0x40, %edx
-	outb %al, %dx
-	movb %ah, %al
-	outb %al, %dx
-
-# setup timer & system call interrupt descriptors.
-	movl $0x00080000, %eax	
-	movw $timer_interrupt, %ax
-	movw $0x8E00, %dx
-	movl $0x20, %ecx
-	lea idt(,%ecx,8), %esi
-	movl %eax,(%esi) 
-	movl %edx,4(%esi)
-	movw $system_interrupt, %ax
-	movw $0xef00, %dx
-	movl $0x80, %ecx
-	lea idt(,%ecx,8), %esi
-	movl %eax,(%esi) 
-	movl %edx,4(%esi)
-
-# unmask the timer interrupt.
-	movl $0x21, %edx
-	inb %dx, %al
-	andb $0xfe, %al
-	outb %al, %dx
-
-# Move to user mode (task 0)
-	pushfl
-	andl $0xffffbfff, (%esp)
-	popfl
-	movl $TSS0_SEL, %eax
-	ltr %ax
-	movl $LDT0_SEL, %eax
-	lldt %ax 
-	movl $0, current
-	sti
-	pushl $0x17
-	pushl $stack0_ptr
-	pushfl
-	pushl $0x0f
-	pushl $task0
-	iret
-
-/****************************************/
-setup_gdt:
-	lgdt lgdt_opcode
-	ret
-
+/*
+ *  setup_idt
+ *
+ *  sets up a idt with 256 entries pointing to
+ *  ignore_int, interrupt gates. It then loads
+ *  idt. Everything that wants to install itself
+ *  in the idt-table may do so themselves. Interrupts
+ *  are enabled elsewhere, when we can be relatively
+ *  sure everything is ok. This routine will be over-
+ *  written by the page tables.
+ */
 setup_idt:
 	lea ignore_int,%edx
 	movl $0x00080000,%eax
 	movw %dx,%ax		/* selector = 0x0008 = cs */
 	movw $0x8E00,%dx	/* interrupt gate - dpl=0, present */
-	lea idt,%edi
+
+	lea _idt,%edi
 	mov $256,%ecx
 rp_sidt:
 	movl %eax,(%edi)
@@ -109,204 +61,115 @@ rp_sidt:
 	addl $8,%edi
 	dec %ecx
 	jne rp_sidt
-	lidt lidt_opcode
+	lidt idt_descr
 	ret
 
-# in: %eax - logic addr; %ebx = base addr ; 
-# %ecx - table addr; %edi - descriptors offset.
-set_base:
-	addl %ebx, %eax
-	addl %ecx, %edi
-	movw %ax, 2(%edi)
-	rorl $16, %eax
-	movb %al, 4(%edi)
-	movb %ah, 7(%edi)
-	rorl $16, %eax
+/*
+ *  setup_gdt
+ *
+ *  This routines sets up a new gdt and loads it.
+ *  Only two entries are currently built, the same
+ *  ones that were built in init.s. The routine
+ *  is VERY complicated at two whole lines, so this
+ *  rather long comment is certainly needed :-).
+ *  This routine will beoverwritten by the page tables.
+ */
+setup_gdt:
+	lgdt gdt_descr
 	ret
 
-write_char:
-	push %gs
-	pushl %ebx
-	pushl %eax
-	mov $0x18, %ebx
-	mov %bx, %gs
-	movl scr_loc, %bx
-	shl $1, %ebx
-	movb %al, %gs:(%ebx)
-	shr $1, %ebx
-	incl %ebx
-	cmpl $2000, %ebx
-	jb 1f
-	movl $0, %ebx
-1:	movl %ebx, scr_loc	
-	popl %eax
-	popl %ebx
-	pop %gs
-	ret
+.org 0x1000
+pg0:
 
-/***********************************************/
+.org 0x2000
+pg1:
+
+.org 0x3000
+pg2:		# This is not used yet, but if you
+		# want to expand past 8 Mb, you'll have
+		# to use it.
+
+.org 0x4000
+after_page_tables:
+	pushl $0		# These are the parameters to main :-)
+	pushl $0
+	pushl $0
+	pushl $L6		# return address for main, if it decides to.
+	pushl $_main
+	jmp setup_paging
+L6:
+	jmp L6			# main should never return here, but
+				# just in case, we know what happens.
+
 /* This is the default interrupt "handler" :-) */
 .align 2
 ignore_int:
-	push %ds
-	pushl %eax
-	movl $0x10, %eax
-	mov %ax, %ds
-	movl $67, %eax            /* print 'C' */
-	call write_char
-	popl %eax
-	pop %ds
-	iret
+	incb 0xb8000+160		# put something on the screen
+	movb $2,0xb8000+161		# so that we know something
+	iret				# happened
 
-/* Timer interrupt handler */ 
+
+/*
+ * Setup_paging
+ *
+ * This routine sets up paging by setting the page bit
+ * in cr0. The page tables are set up, identity-mapping
+ * the first 8MB. The pager assumes that no illegal
+ * addresses are produced (ie >4Mb on a 4Mb machine).
+ *
+ * NOTE! Although all physical memory should be identity
+ * mapped by this routine, only the kernel page functions
+ * use the >1Mb addresses directly. All "normal" functions
+ * use just the lower 1Mb, or the local data space, which
+ * will be mapped to some other place - mm keeps track of
+ * that.
+ *
+ * For those with more memory than 8 Mb - tough luck. I've
+ * not got it, why should you :-) The source is here. Change
+ * it. (Seriously - it shouldn't be too difficult. Mostly
+ * change some constants etc. I left it at 8Mb, as my machine
+ * even cannot be extended past that (ok, but it was cheap :-)
+ * I've tried to show which constants to change by having
+ * some kind of marker at them (search for "8Mb"), but I
+ * won't guarantee that's all :-( )
+ */
 .align 2
-timer_interrupt:
-	push %ds
-	pushl %edx
-	pushl %ecx
-	pushl %ebx
-	pushl %eax
-	movl $0x10, %eax
-	mov %ax, %ds
-	movb $0x20, %al
-	outb %al, $0x20
-	movl $1, %eax
-	cmpl %eax, current
-	je 1f
-	movl %eax, current
-	ljmp $TSS1_SEL, $0
-	jmp 2f
-1:	movl $0, current
-	ljmp $TSS0_SEL, $0
-2:	popl %eax
-	popl %ebx
-	popl %ecx
-	popl %edx
-	pop %ds
-	iret
-
-/* system call handler */
-.align 2
-system_interrupt:
-	push %ds
-	pushl %edx
-	pushl %ecx
-	pushl %ebx
-	pushl %eax
-	movl $0x10, %edx
-	mov %dx, %ds
-	call write_char
-	popl %eax
-	popl %ebx
-	popl %ecx
-	popl %edx
-	pop %ds
-	iret
-
-/*********************************************/
-current:.long 0
-scr_loc:.long 0
+setup_paging:
+	movl $1024*3,%ecx
+	xorl %eax,%eax
+	xorl %edi,%edi			/* pg_dir is at 0x000 */
+	cld;rep;stosl
+	movl $pg0+7,_pg_dir		/* set present bit/user r/w */
+	movl $pg1+7,_pg_dir+4		/*  --------- " " --------- */
+	movl $pg1+4092,%edi
+	movl $0x7ff007,%eax		/*  8Mb - 4096 + 7 (r/w user,p) */
+	std
+1:	stosl			/* fill pages backwards - more efficient :-) */
+	subl $0x1000,%eax
+	jge 1b
+	xorl %eax,%eax		/* pg_dir is at 0x0000 */
+	movl %eax,%cr3		/* cr3 - page directory start */
+	movl %cr0,%eax
+	orl $0x80000000,%eax
+	movl %eax,%cr0		/* set paging (PG) bit */
+	ret			/* this also flushes prefetch-queue */
 
 .align 2
 .word 0
-lidt_opcode:
+idt_descr:
 	.word 256*8-1		# idt contains 256 entries
-	.long idt + KRN_BASE	# This will be rewrite by code. 
+	.long _idt
 .align 2
 .word 0
-lgdt_opcode:
-	.word (end_gdt-gdt)-1	# so does gdt 
-	.long gdt + KRN_BASE	# This will be rewrite by code.
+gdt_descr:
+	.word 256*8-1		# so does gdt (not that that's any
+	.long _gdt		# magic number, but it works for me :^)
 
 	.align 3
-idt:	.fill 256,8,0		# idt is uninitialized
+_idt:	.fill 256,8,0		# idt is uninitialized
 
-gdt:	.quad 0x0000000000000000	/* NULL descriptor */
-	.quad 0x00c09a01000007ff	/* 8Mb 0x08, base = 0x10000 */
-	.quad 0x00c09201000007ff	/* 8Mb 0x10 */
-	.quad 0x00c0920b80000002	/* screen 0x18 - for display */
-
-	.quad 0x0000e90100000068	# TSS0 descr 0x20
-	.quad 0x0000e20100000040	# LDT0 descr 0x28
-	.quad 0x0000e90100000068	# TSS1 descr 0x30
-	.quad 0x0000e20100000040	# LDT1 descr 0x38
-end_gdt:
-	.fill 128,4,0
-stack_ptr:
-	.long stack_ptr
-	.word 0x10
-
-/*************************************/
-.align 3
-ldt0:	.quad 0x0000000000000000
-	.quad 0x00c0fa01000003ff	# 0x0f, base = 0x10000
-	.quad 0x00c0f201000003ff	# 0x17
-tss0:
-	.long 0 			/* back link */
-	.long stack0_krn_ptr, 0x10	/* esp0, ss0 */
-	.long 0, 0			/* esp1, ss1 */
-	.long 0, 0			/* esp2, ss2 */
-	.long 0				/* cr3 */
-	.long task0			/* eip */
-	.long 0x200			/* eflags */
-	.long 0, 0, 0, 0		/* eax, ecx, edx, ebx */
-	.long stack0_ptr, 0, 0, 0	/* esp, ebp, esi, edi */
-	.long 0x17,0x0f,0x17,0x17,0x17,0x17 /* es, cs, ss, ds, fs, gs */
-	.long LDT0_SEL			/* ldt */
-	.long 0x8000000			/* trace bitmap */
-
-	.fill 128,4,0
-stack0_krn_ptr:
-	.long 0
-
-/************************************/
-.align 3
-ldt1:	.quad 0x0000000000000000
-	.quad 0x00c0fa01000003ff	# 0x0f, base = 0x10000
-	.quad 0x00c0f201000003ff	# 0x17
-tss1:
-	.long 0 			/* back link */
-	.long stack1_krn_ptr, 0x10	/* esp0, ss0 */
-	.long 0, 0			/* esp1, ss1 */
-	.long 0, 0			/* esp2, ss2 */
-	.long 0				/* cr3 */
-	.long task1			/* eip */
-	.long 0x200			/* eflags */
-	.long 0, 0, 0, 0		/* eax, ecx, edx, ebx */
-	.long stack1_ptr, 0, 0, 0	/* esp, ebp, esi, edi */
-	.long 0x17,0x0f,0x17,0x17,0x17,0x17 /* es, cs, ss, ds, fs, gs */
-	.long LDT1_SEL			/* ldt */
-	.long 0x8000000			/* trace bitmap */
-
-	.fill 128,4,0
-stack1_krn_ptr:
-	.long 0
-
-/************************************/
-task0:
-	movl $0x17, %eax
-	movw %ax, %ds
-	movl $65, %al              /* print 'A' */
-	int $0x80
-	movl $0xfff, %ecx
-1:	loop 1b
-	jmp task0 
-
-	.fill 128,4,0
-stack0_ptr:
-	.long 0
-
-task1:
-	movl $0x17, %eax
-	movw %ax, %ds
-	movl $66, %al              /* print 'B' */
-	int $0x80
-	movl $0xfff, %ecx
-1:	loop 1b
-	jmp task1
-
-	.fill 128,4,0 
-stack1_ptr:
-	.long 0
-
-/*** end ***/
+_gdt:	.quad 0x0000000000000000	/* NULL descriptor */
+	.quad 0x00c09a00000007ff	/* 8Mb */
+	.quad 0x00c09200000007ff	/* 8Mb */
+	.quad 0x0000000000000000	/* TEMPORARY - don't use */
+	.fill 252,8,0			/* space for LDT's and TSS's etc */
